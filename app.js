a254c71b1385c86ac121842ed2c81493d8c2c672
fsabrantes <fsabrantes96@gmail.com>
2023-02-11 11:19:57 -0300

    teste deploy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express    = require('express');
 const exphbs     = require('express-handlebars');
 const app        = express();
+const port       = process.env.PORT || 4000
 const path       = require('path');
 const db         = require('./db/connection');
 const bodyParser = require('body-parser');
